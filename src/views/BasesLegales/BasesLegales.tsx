14c57ba539bbe2fea0edc7c878b7fd87ed4562d3
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-06-29 16:36:56 -0400

    [fix] bases legales

--- src/views/BasesLegales/BasesLegales.tsx
+++ src/views/BasesLegales/BasesLegales.tsx
@@ -7,20 +7,168 @@ const BasesLegales = () => {
 
   return (
     <div className="bl-section">
-      <h3 className="bl-title">Bases Legales</h3>
+      <h3 className="bl-title">BASES, REQUISITOS Y CONDICIONES DEL SORTEO</h3>
       <BLDownloads />
       <p className="bl-description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quaerat dolores recusandae iure ea illo. 
-        Eius repudiandae voluptatem dolorem dolor expedita suscipit aliquid soluta! Facere repudiandae ipsam amet omnis officiis!
+        En Santiago de chile, a 25-06-2024, comparece RAUL WLADIMIR PANTOJA
+        CEA, cédula de identidad número dieciséis millones doscientos sesenta y cinco mil
+        ciento noventa y ocho guion seis, chileno,casado, constructor, representante legal
+        de empresa Cronomet Inversiones e Ingeniería SpA, rut setenta y siete millones
+        ochocientos cuarenta y seis mil trecientos cuarenta y siete guion cuatro,
+        domiciliado para estos efectos en calle Morandé 835 oficina 518 piso 5, Comuna
+        de Santiago, en adelante “El promotor” o “El organizador”, indistintamente, viene
+        en establecer las bases, requisitos y condiciones en atención a las que se regirá la
+        campaña publicitaria y sorteo artículos tecnológicos, sorteo de propiedades, sorteo
+        de vehículos motorizados, las que se entregaran distintas fechas de sorteo que
+        más adelante se individualiza, denominada “SORTEO DE ORO”, “SORTEO DE
+        PLATA” y “SORTEO DE BRONCE” dirigida a convocar la participación de toda
+        persona interesada en ello, mediante la compra de uno o más boletos, y que
+        conoce y acepta las presentes bases, bajo las cláusulas que, a continuación, se
+        indican:
       </p>
+
       <p className="bl-description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quaerat dolores recusandae iure ea illo. 
-        Eius repudiandae voluptatem dolorem dolor expedita suscipit aliquid soluta! Facere repudiandae ipsam amet omnis officiis!
+        <strong>PRIMERO. DEFINICIONES:</strong> <br />
+        a- PARTICIPANTE: Toda persona natural, con cédula de
+        identidad vigente, que compre o adquiera uno más boletos para participar en el
+        proceso.
+        b- GANADOR: Aquel participante que, de acuerdo al sistema aleatorio de
+        elección, obtenga un resultado positivo, resultando legítimo acreedor del premio
+        descrito en la cláusula QUINTA.
+        c- SISTEMA ALEATORIO DE ELECCIÓN: Sistema
+        de elección, digital o manual, que asegure la elección imparcial, transparente y
+        aleatoria del ganador del concurso, mediante la asignación de un número de serie
+        a cada boleto emitido asociado a un participante.
+        d- PROMOTOR U ORGANIZADOR: persona encargada de dirigir y administrar el proceso de
+        promoción, sorteo y transferencia al ganador del vehículo objeto del concurso;
+        e-PROPIETARIO: Persona que al momento del sorteo acredite tener la calidad de
+        dueño del premio mediante facturas de compra.
+        <br />
+        <strong>SEGUNDO. REQUISITOS PARA PARTICIPAR EN EL CONCURSO:</strong><br />
+        Podrán participar personas naturales, chilenos o extranjeros, con cédula de identidad
+        vigente, que hayan comprado uno o más boletos para participar del presente
+        concurso, directamente desde la página web www.grilloven.cl, y que no se
+        encuentren afectos a una de las inhabilidades descritas en la cláusula SÉPTIMA.
       </p>
+
       <p className="bl-description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quaerat dolores recusandae iure ea illo. 
-        Eius repudiandae voluptatem dolorem dolor expedita suscipit aliquid soluta! Facere repudiandae ipsam amet omnis officiis!
+        <strong>TERCERO. TITULARIDAD DEL BOLETO: </strong><br />
+        El boleto y su correspondiente comprobante de pago constituyen copulativamente
+        un título nominativo que sólo podrá hacer efectivo la persona a cuyo nombre se encuentre
+        registrada a compra del boleto, o su mandatario, o sus herederos, siendo necesaria la
+        exhibición de ambos comprobantes para solicitar y exigir la adjudicación del premio del concurso.
       </p>
+
+      <p className="bl-description">
+      <strong>CUARTO. TÉRMINO PARA HACER VALER BOLETO GANADOR: </strong><br />
+        El término para hacer efectivo el boleto ganador, corresponde a treinta días corridos desde la
+        publicación del ganador del sorteo en la página web www.grilloven.cl o en las
+        RRSS del promotor, ello sin perjuicio de los otros medios de publicidad que hayan
+        de utilizarse en el concurso, y de la notificación por correo electrónico o contacto
+        telefónico que El Organizador establezca con el ganador.
+      </p>
+
+      <p className="bl-description">
+        <strong>QUINTO. PREMIO: </strong> <br />
+        Los ganadores del sorteo, conforme a las presentes bases,
+        recibirá como premio:
+        1. Premio para el ganador. El Ganador puede elegir entre Iphone 15 Pro max
+        de 256 GB o dinero $1.100.000 en efectivo o transferencia bancaria.
+        2. Si se llega a los 10.000 números vendidos El Ganador puede elegir entre
+        una YAMAHA R3A o 5 Millones de Pesos
+        3. Si se llega a los 20.000 números vendidos El Ganador puede elegir entre un
+        Suzuki
+        Swift
+        1.2
+        GL,
+        una
+        YAMAHA
+        R7
+        $9.00.000.
+
+        Todo libre de prohibiciones, gravámenes y multas al momento de la entrega, siendo
+        asimismo de cargo del Organizador el pago de los gastos asociados a la transferencia
+        en su totalidad.
+        Si el ganador esta fuera de Chile y no puede venir a buscar el premio, solo podrá
+        tener la opción del dinero, el cual se le depositará en su cuenta de PAYPAL.
+        La entrega se realizará a continuación de realizada la transferencia al ganador, la
+        que deberá realizarse en el término de treinta días corridos desde realizado el sorteo.
+      </p>
+
+      <p className="bl-description">
+        <strong>SEXTO. CONDICIONES PARA LA REALIZACIÓN DEL SORTEO:</strong><br />
+         a- El
+        Organizador dispondrá la venta boletos con un valor ascendente a $2.500 (dos mil
+        quinientos pesos) cada uno o 4 boletos por $9.000 (nueve mil pesos) que
+        permanecerán a la venta hasta el 15 cada mes según agenda de sorteos;
+        pudiéndose extender el período de venta hasta por 1 mes adicional. b- Si dentro
+        del término anterior la venta de boletos no sobrepasare las mil unidades, el
+        Organizador
+        del
+        concurso
+        sorteará
+        el
+        importe
+        recaudado,
+        publicando
+        oportunamente el total de unidades vendidas, certificadas por Notario Público,
+        reservándose el veinte por ciento del total, con concepto de gastos de
+        administración del concurso c- Sin perjuicio de lo anterior, con motivo de un caso
+        fortuito o fuerza mayor, el Organizador podrá poner término anticipado al concurso
+        o dejarlo sin efecto, procediéndose en tal caso informar dicha circunstancia en la
+        página web que publicita el concurso, y a la devolución del noventa por ciento de lo
+        pagado a cada participante, en el plazo de treinta días corridos desde la
+        publicación respectiva; d- El sorteo se realizará de manera pública y se transmitirá
+        por cualquier medio audiovisual idóneo.
+      </p>
+
+      <p className="bl-description">
+        <strong>  SÉPTIMO. EXCLUSIONES E INHABILIDADES:</strong><br />
+        Queda excluido de participar en
+        el concurso El Organizador y sus parientes hasta el primer grado de
+        consanguineidad y segundo de afinidad inclusive. En caso de contravención, si éste
+        o sus parientes resultaren ganadores, el sorteo podrá ser anulado a solicitud de
+        quien se viere afectado, debiéndose proceder a un nuevo sorteo, dentro de treinta
+        días corridos de declarada la nulidad del sorteo viciado.
+      </p>
+
+      <p className="bl-description">
+        <strong>  OCTAVO. MODIFICACIÓN DE LAS BASES:</strong><br />
+        El organizador, en caso de ser
+        necesario, podrá aclarar, rectificar o enmendar las presentes bases, incluso una vez
+        iniciado el sorteo, debiendo publicar cualquier alteración, conjuntamente con las
+        bases, en la página web que promociona el concurso.
+      </p>
+
+      <p className="bl-description">
+        <strong>NOVENO. ACEPTACIÓN DEL CONTENIDO DE LAS BASES:</strong><br />
+        La sola compra o
+        adquisición de uno o más boletos por parte de los participantes importa la aceptación
+        de éstos de las bases y condiciones del concurso, y de cada una de las cláusulas
+        descritas en el presente instrumento.
+      </p>
+
+      <p className="bl-description">
+        <strong>“DÉCIMA. DERECHO A RETRACTO VALIDO HASTA 72HRS ANTES DEL
+        SORTEO:</strong><br /> Cada participante podrá solicitar la restitución del dinero pagado por la
+        compra de uno o más boletos como último plazo 72hrs antes del sorteo y dentro del
+        término de veinticuatro horas siguientes al pago. Para hacer efectiva la devolución,
+        el participante, a través del correo electrónico raulwpc@gmail.com, deberá
+        informar su derecho a retracto, indicando su nombre, cedula nacional de identidad,
+        número del pedido cuya devolución solicita, correo electrónico y número de
+        contacto, adjuntando además los datos de cuenta para depósito o transferencia
+        electrónica del importe a restituir”.
+      </p>
+
+      <p className="bl-description">
+        Atte.
+      </p>
+
+      <p className="bl-description">Raul Wladimir Pantoja Cea</p>
+      <p className="bl-description">Representante Legal</p>
+      <p className="bl-description">Cronomet Inversiones e Ingeniería SpA</p>
+      <p className="bl-description">Rut 77.846.347-4.</p>
+
       <button className="bl-button" onClick={() => navigate('/')}>Volver a la página principal</button>
     </div>
   )
